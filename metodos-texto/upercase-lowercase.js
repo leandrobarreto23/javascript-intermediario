e4e5500e84de53e239ef421d46bef4591f150f4b
Leandro Barreto <leandro.barreto.lb23@outlook.com>
2025-02-28 08:13:55 -0300

    add upercase-lowercase

---
+++ metodos-texto/upercase-lowercase.js
@@ -0,0 +1,9 @@
+let message = "Estou estudando os fundamentos do Javascript";
+
+console.log(message)
+
+// exibir texto em maiusculo
+console.log(message.toUpperCase());
+
+// exibir texto em minusculo
+console.log(message.toLowerCase());
